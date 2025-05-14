0d1e24043de319d555cae1d49fb12c159c9c9ad0
taruntelitech <taruntelitech@gmail.com>
2025-05-14 10:09:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,37 +12,6 @@ function closeForm() {
 }
 
 
-document.getElementById("buyForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  const form = e.target;
-  const data = {
-    name: form.name.value,
-    phone: form.phone.value,
-    email: form.email.value,
-    address: form.address.value,
-  };
-
-  // URL of your Google Apps Script Web App
-  const scriptURL = "https://script.google.com/macros/s/AKfycbzUjXMGyJJnhBqJrxLN14Lf0DJk7tmLbJ6HUyMwVHrvFMWWyI3mBx6sTyAbi9UyFMeq/exec";
-
-  // Send data to the Google Script Web App using Fetch API
-  fetch(scriptURL, {
-    method: "POST",
-    body: new URLSearchParams(data),  // Convert data to URL-encoded format
-  })
-  .then(res => res.json())
-  .then(response => {
-    alert("Order submitted successfully!");
-    closeForm();
-    form.reset();
-  })
-  .catch(error => {
-    alert("Something went wrong!");
-    console.error(error);
-  });
-});
-
 // Search function
 function searchProducts() {
   const searchTerm = document.getElementById("searchBox").value.toLowerCase();
